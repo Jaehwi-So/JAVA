f81f30cf83733e048c57be9b4cd8376007a0c60e
Jaehwi-So <57826925+Jaehwi-So@users.noreply.github.com>
2020-01-13 17:17:19 +0900

    lamdaxx

---
+++ 22. Lamda expression & Functional Programing/Number.java
@@ -0,0 +1,8 @@
+package lamdaxx;
+
+public interface Number {
+	int getMax(int num1, int num2);
+	//int add(int num1, int num2); 람다식은 이름이 없는 익명 함수로 구현한다. 
+	//인터페이스에 메서드가 여러 개가 있다면 어떤 메서드를 구현한 지 모호하다. 따라서 하나의 메서드만 선언한 인터페이스만 구현 가능하다.
+
+}
---
+++ 22. Lamda expression & Functional Programing/StringConcat.java
@@ -0,0 +1,6 @@
+package lamdaxx;
+
+public interface StringConcat {
+	public void makeString(String s, String s2);
+
+}
