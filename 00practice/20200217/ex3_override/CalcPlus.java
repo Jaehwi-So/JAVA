96d1d71e9fc9c6750d580f74a54ec7cfbd71764f
Jaehwi-So <57826925+Jaehwi-So@users.noreply.github.com>
2020-02-20 07:07:10 +0900

    del

--- 00practice/20200217/ex3_override/CalcPlus.java
+++
@@ -1,8 +0,0 @@
-package ex3_override;
-
-public class CalcPlus extends Calculator{
-	@Override
-	public int getResult(int a, int b) {
-		return a + b;
-	}
-}
--- 00practice/20200217/ex3_override/Calculator.java
+++
@@ -1,7 +0,0 @@
-package ex3_override;
-
-public class Calculator {
-	public int getResult(int a, int b) {
-		return -1;
-	}
-}
